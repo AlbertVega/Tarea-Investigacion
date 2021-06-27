67061543d349bae9c26109ff6016491623e82ff4
cecilianomeibel <85046833+cecilianomeibel@users.noreply.github.com>
2021-06-27 17:11:30 +0200

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -12,14 +12,38 @@ public class Calculadora {
         
         //CALCULATE THE SINUS
         result = Math.sin(AngleInRadians);
-        System.out.println("El seno de " + AngleInDegrees + "° = " + AngleInRadians);
+        System.out.println("El seno de " + AngleInDegrees + "° = " + result);
 
         //CALCULATE THE COSINE
         result = Math.cos(AngleInRadians);
-        System.out.println("El coseno de " + AngleInDegrees + "° = " + AngleInRadians);
+        System.out.println("El coseno de " + AngleInDegrees + "° = " + result);
 
         //CALCULATE THE TANGENT
         result = Math.tan(AngleInRadians);
-        System.out.println("La tangente de " + AngleInDegrees + "° = " + AngleInRadians);
+        System.out.println("La tangente de " + AngleInDegrees + "° = " + result);
+
+        //CALCULATE THE ARCSIN
+       result = Math.asin(AngleInRadians);
+       System.out.println("El arcseno de " + AngleInDegrees + "° = " + result);
+
+        //CALCULATE THE ARCCOS
+       result = Math.acos(AngleInRadians);
+       System.out.println("El arccoseno de " + AngleInDegrees + "° = " + result);
+
+        //CALCULATE THE ARCTAN
+        result = Math.atan(AngleInRadians);
+        System.out.println("La arctangente de " + AngleInDegrees + "° = " + result);
+
+        //CALCULATE THE SINH
+        result = Math.sinh(AngleInRadians);
+        System.out.println("El seno hiperbolico de " + AngleInDegrees + "° = " + result);
+
+        //CALCULATE THE COSH
+        result = Math.cosh(AngleInRadians);
+        System.out.println("El coseno hiperbolico  de " + AngleInDegrees + "° = " + result);
+
+        //CALCULATE THE TANH
+        result = Math.tanh(AngleInRadians);
+        System.out.println("La tangente hiperbolica de " + AngleInDegrees + "° = " + result);
     }
 }
